c84418b4f5f71da8cef01d1efd21f494c08827f5
Jen-Agyemang <jenagyemang@outlook.com>
2019-01-14 14:46:49 -0800

    add
    update

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do a boolean comparison using a while statement.
+            
+           // IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool inputs = Convert.ToBoolean(number);
+
+            switch (number)
+
+            {
+                case 1:
+                    Console.WriteLine("false");
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("False");
+                    Console.ReadLine();
+                    break;
+
+                case 3:
+                    Console.WriteLine("True!");
+                    Console.ReadLine();
+                    break;
+
+
+                default:
+                    Console.WriteLine("False.");
+                    Console.ReadLine();
+
+
+                    break;
+
+                    
+                    
+
+            }
+        }
+    }
+}
+
